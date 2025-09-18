1538a7f7a3863280dbf9d60ee22728fbb58cd145
Renato Figueiredo <renatofigueiredotx@gmail.com>
2025-09-18 18:10:55 -0300

    fix: corrige problema da chave de API no Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,10 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    
+
     return {
       plugins: [react()],
       base: './',
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
